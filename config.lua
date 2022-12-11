758451669baf0cdb9ef4ce234dbe23b9bfe3b626
JustRobb-Development <119083177+JustRobb-Development@users.noreply.github.com>
2022-12-11 22:07:28 +0000

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,27 @@
+QB = {}
+
+QB.Spawns = {
+    ["altast"] = {
+        coords = vector4(-206.11, -1013.32, 30.14, 342.67),
+        location = "altast",
+        label = "Alta Street",
+    },
+
+    ["impound"] = {
+        coords = vector4(-191.43, -1169.58, 23.16, 187.6),
+        location = "impound",
+        label = "Impound",
+    },
+
+    ["delperropier"] = {
+        coords = vector4(-1614.37, -1037.46, 13.15, 231.57),
+        location = "delperropier",
+        label = "Del Perro Pier",
+    },
+
+    ["casino"] = {
+        coords = vector4(884.01, 7.51, 78.91, 321.08),
+        location = "casino",
+        label = "Casino",
+    },
+}
